f5d52d51bd244dc6216bc6cf3b44306c7dd57521
lusiulis <lusiulis@gmail.com>
2024-10-18 17:45:13 -0600

    base

---
+++ API/settings.py
@@ -0,0 +1,15 @@
+# settings.py
+
+import os
+
+# Replace the DATABASES setting with the following:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
+    }
+}
